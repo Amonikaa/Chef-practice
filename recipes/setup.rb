501886ee6a0279676e4fde65f62d4e9762451666
root <root@c-t.c.kubernetes-177105.internal>
2018-01-30 08:52:27 +0000

    initial workstation cookbook commit

---
+++ recipes/setup.rb
@@ -0,0 +1,17 @@
+package 'ntp' do 
+  action :install
+end
+ 
+package 'tree' do 
+  action :install
+end
+
+package 'git' do
+  action :install
+end
+
+file '/etc/motd' do 
+  content 'this server is the property of Monika'
+  owner 'root'
+  group 'root'
+end
